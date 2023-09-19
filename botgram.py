91fae78ee8f19ba1f06803b12a20ae1f2996d817
Yuri <xamexavu@hotmail.com>
2023-09-19 15:18:56 -0300

    Add files via upload

---
+++ botgram.py
@@ -0,0 +1,75 @@
+import requests
+import json
+import urllib.request
+import os
+from myigbot import MyIGBot
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
+import math
+
+
+#calling secret variables
+CAT_KEY = os.environ.get("CAT_KEY")
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+
+bot = MyIGBot(USERNAME, PASSWORD)
+
+#get the cats
+url = "https://api.thecatapi.com/v1/images/search?format=json"
+
+payload={}
+headers = {
+  'Content-Type': 'application/json',
+  'x-api-key': CAT_KEY
+}
+
+proxies = {
+  'http': 'http://10.10.1.10:3128',
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, proxies=proxies)
+todos = json.loads(response.text)
+site = todos[0].get('url')
+r = requests.get(site, allow_redirects=True)
+open('gato.jpeg', 'wb').write(r.content)
+
+
+
+def formatImage():
+    base = Image.new('RGB', (1080,1920), (255,255,0))
+    cat = Image.open('gato.jpeg')
+    originalCat = cat.copy()
+
+    #~ Resize and spawn multiple image of cat in the background
+    wPercent = (216/float(originalCat.size[0]))
+    hSize = int((float(originalCat.size[1])*float(wPercent)))
+    smallCat = originalCat.resize((216,hSize), Image.Resampling.LANCZOS)
+
+    #~ Reduce brightness & blur, goal is to put in foreground the main cat
+    smallCat = ImageEnhance.Brightness(smallCat).enhance(.75)
+    smallCat = smallCat.filter(ImageFilter.GaussianBlur(4))
+
+    for i in range( math.ceil(base.size[1]/hSize) ):
+        for j in range( math.ceil(base.size[0]/smallCat.size[0]) ):
+            base.paste(smallCat, (j*smallCat.size[0],i*smallCat.size[1]))
+
+
+    #~ Resize the image to fit, if it's too large (>1000) or too small (<800)
+    if cat.size[0] > 1000 or cat.size[0] < 800:
+        wPercent = (1000/float(cat.size[0]))
+        hSize = int((float(cat.size[1])*float(wPercent)))
+        cat = cat.resize((1000,hSize), Image.Resampling.LANCZOS)
+
+    wPos = int((1080-cat.size[0])/2)
+    hPos = int((1920-cat.size[1])/2)
+
+    base.paste(cat, (wPos, hPos))
+    base.save('gatogram.jpeg', quality=95)
+
+formatImage()
+response = bot.upload_story('gatogram.jpeg')
+
+if response == 200:
+    print("deu certo")
+else:
+    print("não rolou")
