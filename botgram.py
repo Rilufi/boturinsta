0acb222dbab863130f6be27b7322b987ee8c273c
Yuri <xamexavu@hotmail.com>
2023-10-04 15:09:49 -0300

    Update botgram.py

--- botgram.py
+++ botgram.py
@@ -7,97 +7,25 @@ import math
 from instagrapi import Client
 from instagrapi.types import StoryHashtag
 from random import choice
-
-import email
-import imaplib
-import re
-import random
-
-from instagrapi import Client
-from instagrapi.mixins.challenge import ChallengeChoice
+import telebot
 
 #calling secret variables
 CAT_KEY = os.environ.get("CAT_KEY")
 USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
-
-CHALLENGE_EMAIL = os.environ.get("EMAIL_USER")
-CHALLENGE_PASSWORD = os.environ.get("EMAIL_PASS")
-
-IG_USERNAME = USERNAME
-IG_PASSWORD = PASSWORD
-
-
-def get_code_from_email(username):
-    mail = imaplib.IMAP4_SSL("imap.gmail.com")
-    mail.login(CHALLENGE_EMAIL, CHALLENGE_PASSWORD)
-    mail.select("inbox")
-    result, data = mail.search(None, "(UNSEEN)")
-    assert result == "OK", "Error1 during get_code_from_email: %s" % result
-    ids = data.pop().split()
-    for num in reversed(ids):
-        mail.store(num, "+FLAGS", "\\Seen")  # mark as read
-        result, data = mail.fetch(num, "(RFC822)")
-        assert result == "OK", "Error2 during get_code_from_email: %s" % result
-        msg = email.message_from_string(data[0][1].decode())
-        payloads = msg.get_payload()
-        if not isinstance(payloads, list):
-            payloads = [msg]
-        code = None
-        for payload in payloads:
-            body = payload.get_payload(decode=True).decode()
-            if "<div" not in body:
-                continue
-            match = re.search(">([^>]*?({u})[^<]*?)<".format(u=username), body)
-            if not match:
-                continue
-            print("Match from email:", match.group(1))
-            match = re.search(r">(\d{6})<", body)
-            if not match:
-                print('Skip this email, "code" not found')
-                continue
-            code = match.group(1)
-            if code:
-                return code
-    return False
-
-
-def get_code_from_sms(username):
-    while True:
-        code = input(f"Enter code (6 digits) for {username}: ").strip()
-        if code and code.isdigit():
-            return code
-    return None
-
-
-def challenge_code_handler(username, choice):
-    if choice == ChallengeChoice.SMS:
-        return get_code_from_sms(username)
-    elif choice == ChallengeChoice.EMAIL:
-        return get_code_from_email(username)
-    return False
-
-
-def change_password_handler(username):
-    # Simple way to generate a random string
-    chars = list("abcdefghijklmnopqrstuvwxyz1234567890!&£@#")
-    password = "".join(random.sample(chars, 10))
-    return password
-
+tele_user = os.environ.get("TELE_USER")
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+bot = telebot.TeleBot(TOKEN)
 
 #logging with myIG
-#try:
-if __name__ == "__main__":
+try:
   cl = Client(request_timeout=7)
-  cl.challenge_code_handler = challenge_code_handler
-  cl.change_password_handler = change_password_handler
-  cl.login(IG_USERNAME, IG_PASSWORD)
-  print('gato tentando logar')
-#except:
-#  cl = Client(request_timeout=7)
-#  cl.login(USERNAME, PASSWORD)
-#  print('gato logado')
-  
+  cl.login(USERNAME, PASSWORD)
+  print('gato logado')
+except:
+  print('gato deslogado')
+  bot.send_message(tele_user,  'boturinsta com problema')
+  pass
 
 #get the cats
 url = "https://api.thecatapi.com/v1/images/search?format=json&type=jpeg"
