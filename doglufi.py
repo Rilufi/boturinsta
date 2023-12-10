1343aa16c16339edd159c4b9053dce9385164092
Yuri <xamexavu@hotmail.com>
2023-12-10 11:36:26 -0300

    Update doglufi.py

--- doglufi.py
+++ doglufi.py
@@ -31,54 +31,24 @@ bot = telebot.TeleBot(TOKEN)
 
 #logging with myIG
 try:
-  cl = Client(request_timeout=7)
-  cl.login(USUARIO, SENHA)
-  print('dog logado')
-except:
-  bot.send_message(tele_user,  'doglufi com problema')
-  print('dog deslogado')
-  pass
-
-
-def formatImage(image):
-    base = Image.new('RGB', (1080,1920), (255,255,0))
-    cat = Image.open(image)
-    originalCat = cat.copy()
-
-    #~ Resize and spawn multiple image of cat in the background
-    wPercent = (216/float(originalCat.size[0]))
-    hSize = int((float(originalCat.size[1])*float(wPercent)))
-    smallCat = originalCat.resize((216,hSize), Image.LANCZOS)#Resampling.LANCZOS)
-
-    #~ Reduce brightness & blur, goal is to put in foreground the main cat
-    smallCat = ImageEnhance.Brightness(smallCat).enhance(.75)
-    smallCat = smallCat.filter(ImageFilter.GaussianBlur(4))
-
-    for i in range( math.ceil(base.size[1]/hSize) ):
-        for j in range( math.ceil(base.size[0]/smallCat.size[0]) ):
-            base.paste(smallCat, (j*smallCat.size[0],i*smallCat.size[1]))
-
-
-    #~ Resize the image to fit, if it's too large (>1000) or too small (<800)
-    if cat.size[0] > 1000 or cat.size[0] < 800:
-        wPercent = (1000/float(cat.size[0]))
-        hSize = int((float(cat.size[1])*float(wPercent)))
-        cat = cat.resize((1000,hSize), Image.LANCZOS)#Resampling.LANCZOS)
-
-    wPos = int((1080-cat.size[0])/2)
-    hPos = int((1920-cat.size[1])/2)
-
-    base.paste(cat, (wPos, hPos))
-    base.save(image, quality=95)
-
+    cl = Client(request_timeout=7)
+    cl.login(USUARIO, SENHA)
+    print('dog logado')
+except ClientError as e:
+    if e.status_code == 403:
+        print(f"Error during login: {e}")
+        print("Exiting script due to 403 Forbidden error.")
+        exit()
+    else:
+        bot.send_message(tele_user, 'doglufi com problema')
+        print('dog deslogado')
+        pass
 
 #pegar foto de dog
 def get_random_dog(filename: str='temp') -> None:
-
     r = requests.get('https://dog.ceo/api/breeds/image/random')
     rd = json.loads(r.content)
     r2 = requests.get(rd['message'])
-
     with open(filename, 'wb') as image:
         for chunk in r2:
             image.write(chunk)
@@ -88,7 +58,7 @@ insta_string = f""" Dog do dia {data}
 #DogOfTheDay #CachorroDoDia"""
 
 
-max_retries = 5
+max_retries = 3
 retry_count = 0
 
 while retry_count < max_retries:
@@ -102,6 +72,9 @@ while retry_count < max_retries:
         retry_count += 1
         if retry_count < max_retries:
             print(f"Retrying... (Attempt {retry_count}/{max_retries})")
+            if e.status_code == 403:
+                print("Exiting script due to 403 Forbidden error.")
+                break  # Break the loop if 403 Forbidden error occurs during upload
         else:
             print("Max retries reached. Photo upload failed.")
             bot.send_message(tele_user, 'doglufi com problema')
