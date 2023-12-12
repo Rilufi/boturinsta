839c8df4ae0e1535d50c554a71e73359115e89fd
Yuri <xamexavu@hotmail.com>
2023-12-12 09:44:02 -0300

    Update doglufi.py

--- doglufi.py
+++ doglufi.py
@@ -75,6 +75,3 @@ while retry_count < max_retries:
             bot.send_message(tele_user, 'doglufi com problema')
                 print("Exiting script due to 403 Forbidden error.")
                 break  # Break the loop if 403 Forbidden error occurs during upload
-        else:
-            print("Max retries reached. Photo upload failed.")
-            bot.send_message(tele_user, 'doglufi com problema')
