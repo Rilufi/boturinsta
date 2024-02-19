6e9649c4bcdf5d416ab3a55ad8cab15acce5c7d0
Yuri <xamexavu@hotmail.com>
2024-02-19 17:48:29 -0300

    Update follower.py

--- follower.py
+++ follower.py
@@ -1,35 +1,28 @@
-from InstagramAPI import InstagramAPI
+from instagrapi import Client
 import time
 import random
-
 
 #calling secret variables
 username = os.environ.get("USUARIO")
 password = os.environ.get("SENHA")
 
-# Create an InstagramAPI instance
-api = InstagramAPI(username, password)
-api.login()
+# Create an instagrapi client
+client = Client(request_timeout=7)
+client.login(username, password)
 
 # Set the target account or hashtag related to dogs
 target_account = "dogsofinstagram"
 
 # Get the user IDs of accounts related to dogs
-api.searchUsername(target_account)
-user_id = api.LastJson.get('user', {}).get('pk', None)
+user_ids = [user.pk for user in client.user_search(target_account)]
 
 # Like and follow accounts related to dogs with randomized sleep intervals
-if user_id:
-    api.getUserFollowings(user_id)
-    followings = api.LastJson.get('users', [])
-    
-    for user in followings:
-        user_id = user.get('pk')
-        
-        api.like(user_id)
-        time.sleep(random.uniform(15, 30))  # Random sleep between 15 and 30 seconds
-        api.follow(user_id)
-        time.sleep(random.uniform(30, 60))  # Random sleep between 30 and 60 seconds
+for user_id in user_ids:
+    client.user_like(user_id)
+    time.sleep(random.uniform(15, 30))  # Random sleep between 15 and 30 seconds
+    client.user_follow(user_id)
+    time.sleep(random.uniform(30, 60))  # Random sleep between 30 and 60 seconds
 
 # Logout after completing actions
-api.logout()
+client.logout()
+
