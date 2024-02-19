0687e1c7e01fffcc5b33e879e0675c0e856af8ae
Yuri <xamexavu@hotmail.com>
2024-02-19 18:17:30 -0300

    Update follower.py

--- follower.py
+++ follower.py
@@ -8,7 +8,7 @@ username = os.environ.get("USUARIO")
 password = os.environ.get("SENHA")
 
 # Create an instagrapi client
-client = Client(request_timeout=7)
+client = Client(request_timeout=7, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 client.login(username, password)
 
 # Set the target account or hashtag related to dogs
