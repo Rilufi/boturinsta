8a3d9aaec8399eb6de811a1d64dbb681e684571e
Yuri <xamexavu@hotmail.com>
2024-03-24 10:58:16 -0300

    Update doglufi.py

--- doglufi.py
+++ doglufi.py
@@ -18,8 +18,9 @@ today = date.today() # ex 2015-10-31
 data = today.strftime("%d/%m")
 
 #calling secret variables
-USUARIO = os.environ.get("USUARIO")
-SENHA = os.environ.get("SENHA")
+DOG_KEY = os.environ.get("DOG_KEY")
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
 tele_user = os.environ.get("TELE_USER")
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 bot = telebot.TeleBot(TOKEN)
@@ -28,37 +29,51 @@ bot = telebot.TeleBot(TOKEN)
 #logging with myIG
 try:
     cl = Client(request_timeout=7)
-    cl.login(USUARIO, SENHA)
-    print('dog logado')
+    cl.login(USERNAME, PASSWORD)
+    print('cachorro logado')
 except ClientError as e:
     if e.status_code == 403:
         print(f"Error during login: {e}")
         print("Exiting script due to 403 Forbidden error.")
         exit()
     else:
-        bot.send_message(tele_user, 'doglufi com problema')
-        print('dog deslogado')
+        print(f"Error during login: {e}")
+        bot.send_message(tele_user, 'boturinsta com problema')
         pass
 
-#pegar foto de dog
-def get_random_dog(filename: str='temp') -> None:
-    r = requests.get('https://dog.ceo/api/breeds/image/random')
-    rd = json.loads(r.content)
-    r2 = requests.get(rd['message'])
-    with open(filename, 'wb') as image:
-        for chunk in r2:
-            image.write(chunk)
+#get the dogs
+url = "https://api.thedogapi.com/v1/images/search?format=json&type=jpeg"
+payload={}
+headers = {
+  'Content-Type': 'application/json',
+  'x-api-key': DOG_KEY
+}
+proxies = {
+  'http': 'http://10.10.1.10:3128',
+}
+response = requests.request("GET", url, headers=headers, data=payload, proxies=proxies)
+todos = json.loads(response.text)
+site = todos[0].get('url')
+r = requests.get(site, allow_redirects=True)
+open('dog.jpeg', 'wb').write(r.content)
 
-insta_string = f""" Dog do dia {data}
 
-#DogOfTheDay #CachorroDoDia"""
-
+#post the picture
 max_retries = 3
 retry_count = 0
 
 while retry_count < max_retries:
     try:
-        get_random_dog('dog.jpeg')
+        response = requests.request("GET", url, headers=headers, data=payload, proxies=proxies)
+        todos = json.loads(response.text)
+        site = todos[0].get('url')
+        r = requests.get(site, allow_redirects=True)
+        open('dog.jpeg', 'wb').write(r.content)
+
+        insta_string = f""" Dog of the day {data}
+
+#DogOfTheDay #CachorroDoDia"""
+
         cl.photo_upload('dog.jpeg', insta_string)
         print("foto publicada no insta")
         break  # Break the loop if upload is successful
@@ -67,9 +82,9 @@ while retry_count < max_retries:
         retry_count += 1
         if retry_count < max_retries:
             print(f"Retrying... (Attempt {retry_count}/{max_retries})")
-            if "403" in str(e):  # Check if the error message contains "403"
+            if e.status_code == 403:
                 print("Exiting script due to 403 Forbidden error.")
                 break  # Break the loop if 403 Forbidden error occurs during upload
         else:
             print("Max retries reached. Photo upload failed.")
-            bot.send_message(tele_user, 'doglufi com problema')
+            bot.send_message(tele_user, 'boturinsta com problema')
